2b16f39382093146c3527d462dcfa73f6c35d133
Maria Agustina Santucho <asantucho35@gmail.com>
2023-02-06 22:09:06 -0300

    comence dando los estilos iniciales a la aplicacion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import "./styles/styles.css"
+import freeCodeCampLogo from "./images/freecodecamp-logo.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="freecodecamp-logo-container">
+          <img 
+            className='freecodecamp-logo'
+            src={freeCodeCampLogo}
+            alt="Free code camp logo"
+          />
+        </div>
     </div>
   );
 }
